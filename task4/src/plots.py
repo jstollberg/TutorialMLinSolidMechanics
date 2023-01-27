157d696987145997e7aa2d76289f76aa0183af39
Jonathan Stollberg <jonathanstollberg@gmail.com>
2023-01-27 16:35:32 +0100

    add plot export

--- task4/src/plots.py
+++ task4/src/plots.py
@@ -9,18 +9,18 @@ Authors: Henrik Hembrock, Jonathan Stollberg, Dominik K. Klein
 from matplotlib import pyplot as plt
 import numpy as np
 
-colors = np.array([
-        'tab:blue', 'tab:orange', 'tab:green',
-        'tab:red', 'tab:purple', 'tab:brown',
-        'tab:pink', 'tab:gray', 'tab:olive'
+colors = np.array(["#ecbc00", "#324379", "#bf3c3c",
+        # 'tab:blue', 'tab:orange', 'tab:green',
+        # 'tab:red', 'tab:purple', 'tab:brown',
+        # 'tab:pink', 'tab:gray', 'tab:olive'
         ])
 
-def plot_data(eps, eps_dot, sig, omegas, As):
+def plot_data(eps, eps_dot, sig, omegas, As, file=None):
     n = len(eps[0])
     ns = np.linspace(0, 2*np.pi, n)
     
     plt.figure(dpi=600, figsize=(10, 8))
-    plt.title('Data')
+    # plt.title('Data')
     
     plt.subplot(2,2,1)
     for i in range(len(eps)):
@@ -50,15 +50,17 @@ def plot_data(eps, eps_dot, sig, omegas, As):
         plt.xlim([0, 2*np.pi])
         plt.xlabel('time $t$')
         plt.ylabel(r'strain rate $\.{\varepsilon}$')
-        
+       
+    if file is not None:
+        plt.savefig(file, bbox_inches="tight")
     plt.show()
     
-def plot_model_pred(eps, sig, sig_m, omegas, As):
+def plot_model_pred(eps, sig, sig_m, omegas, As, file=None):
     n = len(eps[0])
     ns = np.linspace(0, 2*np.pi, n)
     
     plt.figure(dpi = 600, figsize = (10,4))
-    plt.title('Data: dashed line, model prediction: continuous line')
+    plt.suptitle('Data: dashed line, model prediction: continuous line')
     
     plt.subplot(1,2,1)
     for i in range(len(eps)):
@@ -77,4 +79,6 @@ def plot_model_pred(eps, sig, sig_m, omegas, As):
         plt.xlabel('strain $\\varepsilon$')
         plt.ylabel('stress $\\sigma$')
 
+    if file is not None:
+        plt.savefig(file, bbox_inches="tight")
     plt.show()
